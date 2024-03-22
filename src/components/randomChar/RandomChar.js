486aceb7747b56f8b1931505c6b0a39d7cd0bd77
alex-sly <sly1521@mail.ru>
2024-03-22 18:27:47 +0500

    lesson41

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -7,7 +7,7 @@ import "./randomChar.scss";
 import mjolnir from "../../resources/img/mjolnir.png";
 
 const RandomChar = () => {
-  const [char, setChar] = useState(null);
+  const [char, setChar] = useState({});
   const { loading, error, getCharacter, clearError } = useMarvelService();
 
   useEffect(() => {
